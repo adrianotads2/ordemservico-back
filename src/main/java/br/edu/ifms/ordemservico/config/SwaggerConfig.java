2be777b8a29fefcb5cc2a5787b2ea598ad2aa1fc
adrianotads2 <altairmt@gmail.com>
2021-10-14 21:46:09 -0300

    Correção Swagger

--- src/main/java/br/edu/ifms/ordemservico/config/SwaggerConfig.java
+++ src/main/java/br/edu/ifms/ordemservico/config/SwaggerConfig.java
@@ -13,15 +13,16 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig implements WebMvcConfigurer {
-
+	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html")
-		.addResourceLocations("classpath:/META-INF/resources/");
-		
+	.addResourceLocations("classpath:/META-INF/resources/");
+	
 		registry.addResourceHandler("/webjars/**")
-		.addResourceLocations("classpath:/META-INF/resources/webjars");
+	.addResourceLocations("classpath:/META-INF/resources/webjars/");
 	}
+
 	
 	@Bean
 	public Docket api() {
